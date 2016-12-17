ce194cf357a6b1919ed77b12511bc84cbca737b4
Rinat Enikeev <rinat.enikeev@gmail.com>
2016-12-17 12:52:02 +0500

    initial commit: project created

---
+++ Matrix Depth Rain/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Matrix Depth Rain
+//
+//  Created by Rinat Enikeev on 17/12/2016.
+//  Copyright © 2016 Rinat Enikeev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
